fbfe2d20610ed5cc42ff3d9f998548eb77ae9c3c
Min Min <minmhan@gmail.com>
2016-09-29 20:53:57 +0800

    add static pages

--- catalog/templatetags/catalog_tags.py
+++ catalog/templatetags/catalog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from cart import cart
+from django.contrib.flatpages.models import FlatPage
+
 
 register = template.Library()
 
@@ -8,3 +10,9 @@ register = template.Library()
 def cart_box(request):
     cart_item_count = cart.cart_distinct_item_count(request)
     return { 'cart_item_count': cart_item_count }
+
+
+@register.inclusion_tag("tags/footer.html")
+def footer_links():
+    flatpage_list = FlatPage.objects.all()
+    return { 'flatpage_list': flatpage_list }
