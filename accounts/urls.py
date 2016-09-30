30cad07c7197aaa5e004f8d0137123f2e79c84f3
Min Min <minmhan@gmail.com>
2016-09-30 22:02:05 +0800

    Update account module

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,16 @@
 from ecomstore import settings
 from django.conf.urls import url
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout, password_change
 from . import views
+
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^my_account/$', views.my_account, name='my_account'),
     url(r'^order_details/(?P<order_id>[-\w]+)/$', views.order_details, name='order_details'),
-    url(r'^order_info//$', views.order_info, name='order_info'),
-    url(r'^login/$', views.login, name='login'),
+    url(r'^order_info/$', views.order_info, name='order_info'),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
+    url(r'^accounts/password_change/$', password_change, name='password_change'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,43 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.core import urlresolvers
+
 
 # Create your views here.
-def register():
-    pass
+def register(request, template_name='accounts/register.html'):
+    if request.method == 'POST':
+        postdata = request.POST.copy()
+        form = UserCreationForm(postdata)
+        if form.is_valid():
+            form.save()
+            un = postdata.get('username', '')
+            pwd = postdata.get('password1', '')
+            from django.contrib.auth import login, authenticate
+            new_user = authenticate(username=un, password=pwd)
+            if new_user and new_user.is_active:
+                login(request, new_user)
+                url = urlresolvers.reverse('my_account')
+                return HttpResponseRedirect(url)
+    else:
+        form = UserCreationForm()
+    page_title = 'User Registration'
+    return render(request, template_name, {'form': form, 'page_title':page_title})
 
-def my_account():
-    pass
+
+def my_account(request):
+    page_title = 'My Account'
+    name = request.user.username
+    return render(request, 'accounts/my_account.html', {'page_title':page_title, 'name':name})
+
 
 def order_details():
     pass
 
-def order_info():
-    pass
+
+def order_info(request):
+    return render(request)
+
 
 def login(request):
     return render(request, 'accounts/login.html')
